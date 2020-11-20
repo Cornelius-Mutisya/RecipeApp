de7b6e6057cb636aafba0dc702cc8fee79cd69f3
Cornelius-Mutisya <corneliusmutisya11@gmail.com>
2020-11-20 20:48:23 +0300

    Service for Cross-Component Communication

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,6 +1,9 @@
+import { EventEmitter } from '@angular/core';
 import { Recipe } from './recipe.model';
 
 export class RecipeService {
+    recipeSelected = new EventEmitter<Recipe>();
+
   private recipes: Recipe[] = [
     new Recipe(
       'A test Recipe',
@@ -10,7 +13,7 @@ export class RecipeService {
     new Recipe(
       'Another test Recipe',
       'A second test',
-      'https://i2.wp.com/www.downshiftology.com/wp-content/uploads/2018/12/Shakshuka-19.jpg'
+      'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRAtSsWncpqyTyySN9MfruwODDNsbjNyho25A&usqp=CAU'
     ),
   ];
 
